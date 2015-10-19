b44c71a47491034c04fa29c699ace1225746bf6f
tankandrun <jinshundu@gmail.com>
2015-10-19 21:24:28 +0800

    Initial Commit

---
+++ DouBanJiang/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DouBanJiang
+//
+//  Created by 金顺度 on 15/10/19.
+//  Copyright © 2015年 金顺度. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
